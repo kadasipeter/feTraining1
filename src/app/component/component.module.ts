c69010854f5d8f320002796ca38200317b78bdea
Peter Kadaši <kadasi@kros.sk>
2020-12-09 12:00:27 +0100

    Oprava

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyNavbarComponent } from './my-navbar/my-navbar.component';
-import { AppRoutingModule } from '../app-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { MainComponent } from './main/main.component';
 import { ItemsModule } from './feature/items/items.module';
 import { ExampleModule } from './feature/example/example.module';
 import { NotFoundModule } from './feature/not-found/not-found.module';
+import { AppRoutingModule } from '../app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const COMPONENTS = [
-  MainComponent,
-  MyNavbarComponent,
+  MyNavbarComponent, MainComponent
 ];
 
 @NgModule({
   declarations: [...COMPONENTS],
   imports: [
+    AppRoutingModule,
     CommonModule,
+    ItemsModule,
+    ExampleModule,
+    NotFoundModule,
     SharedModule
   ],
   exports: [...COMPONENTS]
