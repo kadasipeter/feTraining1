fe5c8fb24502fda17e9886155907d512271234f6
Peter Kadaši <kadasi@kros.sk>
2021-01-25 08:09:10 +0100

    Tests for WorkItemService

--- src/app/core/work-item.service.spec.ts
+++ src/app/core/work-item.service.spec.ts
@@ -1,24 +1,92 @@
-import { Store } from '@ngrx/store';
-import { of } from 'rxjs';
-import { AppState } from '../state/app-state';
+import { TestBed } from '@angular/core/testing';
 
+import { of } from 'rxjs';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { StoreModule } from '@ngrx/store';
+
+import { ItemHelperService } from './item-helper.service';
+import { WorkItem } from './work-item.model';
 import { WorkItemService } from "./work-item.service";
+import { WorkItemSelectorsService } from '../state/items/work-items-selectors.service';
+
+const fakeItems: WorkItem[] =[
+  {
+    id: 1,
+    value: 7,
+    description: 'aaa',
+    timestamp: new Date()
+  },
+  {
+    id: 2,
+    value: 8,
+    description: 'abc',
+    timestamp: new Date()
+  },
+  {
+    id: 3,
+    value: 9,
+    description: 'xyz',
+    timestamp: new Date()
+  },
+];
+
+const fakeItem: WorkItem =
+  {
+    id: 10,
+    value: 5,
+    description: 'Test item',
+    timestamp: new Date()
+  };
 
 class StoreMock {
   select =  jasmine.createSpy().and.returnValue(of({}));
   dispatch = jasmine.createSpy();
 }
 
-// describe('ValueService', () => {
-//   const helperServiceMock = jasmine.createSpyObj('item-helper-service', [], [])
-//   const mockStore: Store<{ payload: AppState }> = new StoreMock();
-//   const WorkItemSelectorsServiceMock = jasmine.createSpyObj('WorkItemSelectorsService', ['getAllItems$'], [])
+describe('ValueService', () => {
+  const helperServiceMock = jasmine.createSpyObj('item-helper-service', ['generateWorkItem'])
+  helperServiceMock.generateWorkItem.and.returnValue(fakeItem);
 
-//   let service: WorkItemService;
+  const WorkItemSelectorsServiceMock = jasmine.createSpyObj('WorkItemSelectorsService', ['getAllItems$', 'getItem$'], [])
+  WorkItemSelectorsServiceMock.getAllItems$.and.returnValue(of(fakeItem));
+  WorkItemSelectorsServiceMock.getItem$.and.returnValue(of(fakeItem));
 
-//   beforeEach(() => { service = new WorkItemService(helperServiceMock, mockStore, WorkItemSelectorsServiceMock); });
+  let service: WorkItemService;
 
-//   it('something', () => {
-//     expect(service. ...).toBe('real value');
-//   });
-// });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      StoreModule.forRoot([], {
+        runtimeChecks: {
+          strictStateImmutability: true,
+          strictActionImmutability: true,
+        }
+      })
+    ],
+    providers: [
+      {
+        provide: ItemHelperService,
+        useValue: helperServiceMock
+      },
+      provideMockStore,
+      {
+        provide: WorkItemSelectorsService,
+        useValue: WorkItemSelectorsServiceMock
+      }
+    ]
+  }));
+
+  beforeEach(() => {
+    service = TestBed.inject(WorkItemService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should get work item', () => {
+    expect(service.getWorkItem(1)).toBe(fakeItem);
+  });
+
+
+
+});
